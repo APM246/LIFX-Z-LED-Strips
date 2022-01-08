68796939849db081c326d046dc7c7d67de1ded30
APM246 <apm0366@gmail.com>
2022-01-08 21:26:24 +0800

    removes need for LIFX app

--- cycle.py
+++ cycle.py
@@ -12,6 +12,8 @@ light = MultiZoneLight(MAC_ADDRESS, IP_ADDRESS)
 PATH = "PATH_TO_FOLDER_OF_PHOTOS"
 photos = listdir(PATH)
 photos = map(lambda x: '{0}\\{1}'.format(PATH, x), photos)
+light.set_power(True)
+
 for photo in photos:
 	windll.user32.SystemParametersInfoW(20, 0, photo, 0)
 	matchDesktopBackground(light, photo)
--- main.py
+++ main.py
@@ -5,4 +5,5 @@ MAC_ADDRESS = "YOUR_MAC_ADDRESS"
 IP_ADDRESS = "YOUR_IP_ADDRESS"
 
 light = MultiZoneLight(MAC_ADDRESS, IP_ADDRESS)
+light.set_power(True)
 matchDesktopBackground(light)
