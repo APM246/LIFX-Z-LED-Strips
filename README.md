8a3fb0c7a25ce7f8af32cefa9669d1fd74571b3a
APM246 <apm0366@gmail.com>
2022-06-12 01:58:22 +0800

    enable providing image path as arg and updated README

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Program that sets LIFX Z LED strips to average colour of desktop background
 cycle.py program which cycles through a number of desktop backgrounds.
 
 ![Gif showcasing cycle.py program](https://media.giphy.com/media/LD7Rh4BD7MltAYeW8W/giphy-downsized-large.gif)
+
+## Setup
+1. Run 'pip install requirements.txt'
+2. Replace MAC_ADDRESS AND IP_ADDRESS variables in main.py with corresponding values of your LIFX device.
+3. Run 'python main.py'
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from lib.lifx import matchDesktopBackground
 from lifxlan.multizonelight import MultiZoneLight
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, help='path to image file instead of reading windows registry for current desktop background image')
+args = parser.parse_args()
+
 MAC_ADDRESS = "YOUR_MAC_ADDRESS"
 IP_ADDRESS = "YOUR_IP_ADDRESS"
 
 light = MultiZoneLight(MAC_ADDRESS, IP_ADDRESS)
 light.set_power(True)
-matchDesktopBackground(light)
+matchDesktopBackground(light, args.path)
